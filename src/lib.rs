17ffa81547243ab4d26b81323931c441e0a9b1af
Ljiacheng <Ljiachenggh@gmail.com>
2023-05-30 10:12:52 +0800

    update curv dependency

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,6 @@ version 3 of the License, or (at your option) any later version.
 */
 
 // based on the paper: https://eprint.iacr.org/2017/1066.pdf
-#[macro_use]
 extern crate serde_derive;
 extern crate serde;
 
